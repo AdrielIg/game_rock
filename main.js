bbc22b6d0879972a0489018443a1f2eae117418c
AdrielIg <adrielignaciogomez@gmail.com>
2020-12-17 23:51:52 -0300

    index.html and main.js added

---
+++ main.js
@@ -0,0 +1,24 @@
+function computerPlay() {
+    const options = ["scissors" , "rock" , "paper"];
+    return options[Math.floor(Math.random()* options.length)];
+}
+
+let computerSelection = computerPlay();
+console.log(computerSelection)
+let playerSelection = "paper";
+console.log(playerSelection)
+
+function playRound(playerSelection, computerSelection){
+    //If player win
+    if ((playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "paper")){
+      console.log("The player won");  
+      //If computer win
+}   else if ((playerSelection === "rock" && computerSelection === "paper") || (playerSelection === "scissors" && computerSelection === "rock") || (playerSelection === "paper" && computerSelection === "scissors")){
+        console.log("The computer won");  
+}   // If its a tie
+    else {
+        console.log("It´s a tie!")
+}
+}
+
+playRound();
