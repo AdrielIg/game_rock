d784ca4a512b74aac880d973a924bd3c836ef87a
AdrielIg <adrielignaciogomez@gmail.com>
2020-12-19 20:03:33 -0300

    Problems with functionCHOICE

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-function playRound(playerSelection, computerSelection){ 
+let playerSelection;
+let computerSelection;
+let round;
+let playerScore  = 0;
+let computerScore;
+//CHOICE NO FUNCIONABA ADENTRO DE PLAYROUND PORQUE LAS VARIABLES TENIAN EL MISMO NOMBRE QUE LOS PARAMETROS DE PLAYROUND Y SE SOBREESCRIBIAN LOS VALORES
+
+function playRound(playerChoose, computerChoose){ 
     
         //If player win
     if ((playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "paper")){
-        return `The player won! ${playerSelection} beats ${computerSelection}`;  
-     
+        return `The player won! ${playerSelection} beats ${computerSelection}`; 
     }  //If computer win
     else if ((playerSelection === "rock" && computerSelection === "paper") || (playerSelection === "scissors" && computerSelection === "rock") || (playerSelection === "paper" && computerSelection === "scissors")){
         return `The computer won!  ${computerSelection} beats ${playerSelection}`;  
@@ -11,12 +17,24 @@ function playRound(playerSelection, computerSelection){
     else {
         return "It´s a tie!";
     }
+c
 }
 
 function choice (){
     
-    playerSelection = prompt("Que opcion elegis? Rock, Paper or Scissors?");
-    playerSelection = playerSelection.toLowerCase();
+    playerSelection = prompt("What do you choose? Rock, Paper or Scissor?").toLowerCase();
+    while (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !==  "scissor"){
+        playerSelection = prompt("Write the opcion correctly (Rock, Paper or Scissor):").toLowerCase();
+        if (playerSelection === "paper" && playerSelection === "rock" && playerSelection === "scissor"){
+            playerSelection = playerSelection;
+            break
+        }
+        else{
+            continue
+        }
+    
+     }
+    
     computerSelection = computerPlay();
     console.log(`The computer chose: ${computerSelection}`);
     console.log(`You chose: ${playerSelection}`);
@@ -24,15 +42,14 @@ function choice (){
 } 
 
 function computerPlay() {
-    const options = ["scissors" , "rock" , "paper"];
+    const options = ["scissor" , "rock" , "paper"];
         return options[Math.floor(Math.random()* options.length)];
 }
 
 
-let playerSelection;
-let computerSelection;
+
 
 for (let i = 0 ; i < 5; i++){
-    choice();   
+    choice(); 
     console.log(playRound(playerSelection, computerSelection));
 }
