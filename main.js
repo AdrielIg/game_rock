645e076c3f1c79564762512d84d1f40f103e36ef
AdrielIg <adrielignaciogomez@gmail.com>
2020-12-19 22:11:09 -0300

    Finished console rock game

--- main.js
+++ main.js
@@ -1,55 +1,79 @@
 let playerSelection;
 let computerSelection;
-let round;
-let playerScore  = 0;
-let computerScore;
+let round = 0;
+let playerScore = 0;
+let computerScore = 0;
+
 //CHOICE NO FUNCIONABA ADENTRO DE PLAYROUND PORQUE LAS VARIABLES TENIAN EL MISMO NOMBRE QUE LOS PARAMETROS DE PLAYROUND Y SE SOBREESCRIBIAN LOS VALORES
 
 function playRound(playerChoose, computerChoose){ 
+
     
         //If player win
-    if ((playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "paper")){
+    if ((playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "rock" && computerSelection === "scissor") || (playerSelection === "scissor" && computerSelection === "paper")){
+        playerScore++;
+        round++;
         return `The player won! ${playerSelection} beats ${computerSelection}`; 
+            
     }  //If computer win
-    else if ((playerSelection === "rock" && computerSelection === "paper") || (playerSelection === "scissors" && computerSelection === "rock") || (playerSelection === "paper" && computerSelection === "scissors")){
+    else if ((playerSelection === "rock" && computerSelection === "paper") || (playerSelection === "scissor" && computerSelection === "rock") || (playerSelection === "paper" && computerSelection === "scissor")){
+        computerScore++;
+        round++;
         return `The computer won!  ${computerSelection} beats ${playerSelection}`;  
+            
     }   // If its a tie
     else {
+        round++;
+        computerScore++;
+        playerScore++;
         return "It´s a tie!";
     }
-c
+
+    
+
+
 }
 
-function choice (){
+function playerChoice (){
     
     playerSelection = prompt("What do you choose? Rock, Paper or Scissor?").toLowerCase();
-    while (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !==  "scissor"){
+    while (["scissor" , "rock" , "paper"].indexOf(playerSelection) === -1){
         playerSelection = prompt("Write the opcion correctly (Rock, Paper or Scissor):").toLowerCase();
-        if (playerSelection === "paper" && playerSelection === "rock" && playerSelection === "scissor"){
-            playerSelection = playerSelection;
-            break
-        }
-        else{
-            continue
-        }
-    
-     }
-    
-    computerSelection = computerPlay();
-    console.log(`The computer chose: ${computerSelection}`);
+        
+    }
+
     console.log(`You chose: ${playerSelection}`);
     
+    
 } 
+function computerChoice(){
+    computerSelection = computerPlay();
+    console.log(`The computer chose: ${computerSelection}`);   
+}
 
 function computerPlay() {
     const options = ["scissor" , "rock" , "paper"];
         return options[Math.floor(Math.random()* options.length)];
 }
 
+function finalResult(){
+    if (playerScore === 5){
+        return "PLAYER WINS"
+    }
+    else if (computerScore === 5){
+        return "COMPUTER WINS"
+    }
+}
 
-
-
-for (let i = 0 ; i < 5; i++){
-    choice(); 
-    console.log(playRound(playerSelection, computerSelection));
+if (playerScore <= 5 && computerScore <= 5){
+    while (playerScore < 5 && computerScore < 5){
+        playerChoice();
+        computerChoice(); 
+        console.log(playRound(playerSelection, computerSelection));
+        console.log(`Player´s Points: ${playerScore}`);
+        console.log(`Computer´s Points: ${computerScore}`);  
+    }
+    if (playerScore === 5 || computerScore === 5){
+        console.log(finalResult());
+    }
 }
